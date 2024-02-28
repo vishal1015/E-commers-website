524a8d848fabd4615953f93730b0c293be2321db
vishal1015 <vkharkya9@gmail.com>
2024-02-28 20:19:29 +0530

    addded search drop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import './Header.css'
 import Logo from '../assests/images/logo.png';
+import SearchIcon from "@mui/icons-material/Search";
+import Select from '../components/selectDropdown/Select.js'
 const Header =()=>{
     return (
       <header>
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-sm-2'>
-                    <img src={Logo} alt='logo' />
-                </div>
-                <div className='col-sm-5'> 
-                  <div className='headerSearch d-flex align-items-center'>
-                    <div className='selecrDrop cursor' >
-                     All catgories
-                    </div>
-                    <div className='search'>
-                      <input  type='text'/>
-                    </div>
-                  </div>
-                </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-2">
+              <img src={Logo} alt="logo" />
             </div>
+            <div className="col-sm-5">
+              <div className="headerSearch d-flex align-items-center">
+                <div className="selecrDrop cursor">
+                  All catgories
+                  <Select />
+                  </div>
+                <div className="search">
+                  <input type="text" placeholder="Search for items..." />
+                  <SearchIcon  className='SearchIcon cursor'/>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
-    )
+    );
 }
 export default Header;
---
+++ src/components/selectDropdown/Select.js
@@ -0,0 +1,11 @@
+import '../selectDropdown/Select.css';
+const Select=()=>{
+  return(
+    <>
+    <div className="selectDrop">
+
+    </div>
+    </>
+  )
+}
+export default Select;
